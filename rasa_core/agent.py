9534cb79b818853e9e30ad1d931ca46a714a7581
joeyfaulkner <joeymfaulkner@gmail.com>
2018-08-08 10:57:00 +0200

    added predict endpoint

--- rasa_core/agent.py
+++ rasa_core/agent.py
@@ -23,6 +23,7 @@ from rasa_core.policies.memoization import MemoizationPolicy
 from rasa_core.processor import MessageProcessor
 from rasa_core.tracker_store import InMemoryTrackerStore, TrackerStore
 from rasa_core.trackers import DialogueStateTracker
+import time
 from rasa_core.utils import EndpointConfig
 
 logger = logging.getLogger(__name__)
@@ -54,6 +55,7 @@ class Agent(object):
         self.nlg = NaturalLanguageGenerator.create(generator, self.domain)
         self.tracker_store = self.create_tracker_store(
                 tracker_store, self.domain)
+        self.time_of_last_train = None
 
     @classmethod
     def load(cls,
@@ -264,6 +266,7 @@ class Agent(object):
         logger.debug("Agent trainer got kwargs: {}".format(kwargs))
         check_domain_sanity(self.domain)
 
+        self.time_of_last_train = time.time()
         self.policy_ensemble.train(training_trackers, self.domain,
                                    **kwargs)
 
@@ -295,6 +298,7 @@ class Agent(object):
         logger.debug("Agent online trainer got kwargs: {}".format(kwargs))
         check_domain_sanity(self.domain)
 
+        self.time_of_last_train = time.time()
         self.policy_ensemble.train(training_trackers, self.domain, **kwargs)
 
         ensemble = OnlinePolicyEnsemble(self.policy_ensemble,
@@ -392,9 +396,9 @@ class Agent(object):
             return domain
         else:
             raise ValueError(
-                    "Invalid param `domain`. Expected a path to a domain "
-                    "specification or a domain instance. But got "
-                    "type '{}' with value '{}'".format(type(domain), domain))
+                "Invalid param `domain`. Expected a path to a domain "
+                "specification or a domain instance. But got "
+                "type '{}' with value '{}'".format(type(domain), domain))
 
     @staticmethod
     def create_tracker_store(store, domain):
